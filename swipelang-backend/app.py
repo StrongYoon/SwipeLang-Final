75b1b22a3ab0bec51c4f37ded90b35aaeac7ed50
StrongYoon <2024650019@sdu.ac.kr>
2025-04-18 23:25:47 +0700

    🐛 Fix: app.py 들여쓰기 오류 수정

--- swipelang-backend/app.py
+++ swipelang-backend/app.py
@@ -1,4 +1,4 @@
-    from flask import Flask, jsonify, request, send_file
+from flask import Flask, jsonify, request, send_file
 from utils.data_manager import load_slang_data, load_user_history, save_user_history, get_today_key
 from utils.quiz_generator import generate_quiz
 from utils.tts import speak
